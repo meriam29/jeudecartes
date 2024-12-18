c1c2b572a0cbf46102d7e65e887eb6d3d73b4bac
Zied <zied.benmohamed@externe.bnpparibas.com>
2024-12-18 18:53:15 +0100

    init project

---
+++ src/main/java/com/meriammejri/jeudecarets/JeudecaretsApplication.java
@@ -0,0 +1,13 @@
+package com.meriammejri.jeudecarets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeudecaretsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeudecaretsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meriammejri/jeudecarets/JeudecaretsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meriammejri.jeudecarets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeudecaretsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
